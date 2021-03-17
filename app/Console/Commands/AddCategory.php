716d126cc94d126ac21ec8e7643d95855d815429
ayoubinoss <ayoubaitbahaddou@gmail.com>
2021-03-17 14:16:53 +0100

    Add commands to add product and category

---
+++ app/Console/Commands/AddCategory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Category;
+
+class AddCategory extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'categories:add 
+                            {name : the name of the category}
+                            {parent_category? : the ID of the parent category}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a category to the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $category = new Category;
+        $category->name = $this->argument('name');
+        if($this->argument('parent_category') !== null)
+        {
+            $category->parent_category_id = $this->argument('parent_category');
+        }
+        $category->save();
+    }
+}
---
+++ app/Console/Commands/AddProduct.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Product;
+
+class AddProduct extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'products:add 
+                            {name : The product name} 
+                            {description : The product description} 
+                            {price : The product price} 
+                            {image : The product image location} 
+                            {category* : The id of the categories which the product belongs to}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a product to the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $arguments = $this->arguments();
+        $product = new Product;
+        $product->name = $arguments['name'];
+        $product->description = $arguments['description'];
+        $product->price = $arguments['price'];
+        $product->image = $arguments['image'];
+        $product->save();
+        //insert into the intermediate table ( product_category )
+        try 
+        {
+            foreach($arguments['category'] as $category)
+            {
+                $product->categories()->attach(Category::$category);
+            }
+        } catch(QueryException $e)
+        {
+            
+        }
+        
+    }
+}
